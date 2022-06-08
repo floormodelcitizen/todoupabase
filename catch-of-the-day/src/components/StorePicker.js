fb4c63e31be63e3d8a2cc2e62e313eec08bc24f6
Chad Christopher <devluxv20@gmail.com>
2022-06-07 22:33:38 -0700

    Add App cmp

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 class StorePicker extends React.Component {
   render() {
-    return <p>I am the check!</p>
+    return (
+      <Fragment>
+        <form className="store-selector">
+          <h2>Please enter a store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store &#8594;</button>
+        </form>
+      </Fragment>
+    )
   }
 }
 
