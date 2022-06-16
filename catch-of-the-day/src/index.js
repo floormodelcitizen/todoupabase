0cbd84ee0e900021b733a734314901730e101719
Chad Christopher <devluxv20@gmail.com>
2022-06-15 18:37:51 -0700

    Replace node-pre-gyp13 with @mapbox/node-pre-gyp

--- catch-of-the-day/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import StorePicker from './components/StorePicker'
-import App from './components/App'
-import './css/style.css'
-
-render(<App />, document.querySelector('#main'))
-
-//> Lesson 6
