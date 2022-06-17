4cfcc580b761deae0084beb8f2ac71d9ebea7863
Chad Christopher <devluxv20@gmail.com>
2022-06-16 22:09:55 -0700

    Update index.sv, add Todo slot and each block

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,15 @@
 <script>
 	import TodoForm from '../components/TodoForm.svelte';
+	import { todos } from './../stores/todoStore.js';
+	import Todo from '../components/Todo.svelte';
 </script>
 
 <main>
-	<h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">My Todos</h1>
+	<h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">
+		My Todos
+	</h1>
 	<TodoForm />
+	{#each $todos as todo}
+		<Todo {todo} index={todo.id} />
+	{/each}
 </main>
